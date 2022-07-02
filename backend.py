a279270c12684723e71409fa25cc725653d1e175
RyanAngWeiHao <98933340+RyanAngWeiHao@users.noreply.github.com>
2022-07-03 02:09:43 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -3,10 +3,13 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
-def backend(data):
+def backend():
 #     text = request.form['texts']
 #     text = '3'
-    return data
+    if request.is_json:
+        data = request.get_json()
+        text = data['data']
+        return text
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8010)
