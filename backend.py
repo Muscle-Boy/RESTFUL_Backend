9257b6213d55ac8983b2841e0d54b378c8851fbe
RyanAngWeiHao <98933340+RyanAngWeiHao@users.noreply.github.com>
2022-07-03 01:44:08 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -3,10 +3,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
-def backend():
+def backend(data):
 #     text = request.form['texts']
-    text = '3'
-    return text
+#     text = '3'
+    return data
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8010)
